485e1996ae80cbf3269c1b0c8f2ec5efce6f1701
Tanja Lavrencic <tancka.tancka@gmail.com>
2016-03-23 18:52:58 +0100

    uganka

---
+++ uganka.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+resitev = 40
+ # print (resitev)
+
+ugibaj = int(raw_input("Prosim ugibaj številko: "))
+
+if ugibaj == resitev:
+    print "Bravoooooooooooo! ;)"
+
+elif ugibaj < resitev:
+    print "Ups, prava številka je še višja!:0"
+else:
+    print "Ajoj, prava številka je nižja! :/"
