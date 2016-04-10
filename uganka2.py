affaebd3903316d56816b0b9bbbf5af717788516
Tanja Lavrencic <tancka.tancka@gmail.com>
2016-04-10 21:28:15 +0200

    uganka popravki

--- uganka2.py
+++ uganka2.py
@@ -1,15 +1,17 @@
 # -*- encoding: utf-8 -*-
 resitev = 5
-ugibaj = 0
  # print (resitev)
 
-while resitev != int(ugibaj):
+while True:
 
-    ugibaj = int(raw_input("Izvoli, ugibaj številko:"))
+    ugibaj = int(raw_input("Ugibaj številko:")) #int ne rabis
 
     if ugibaj == resitev:
         print ("Bravoooooooooooo! ;)")
+        break
     elif ugibaj < resitev:
         print ("Ups, prava številka je še višja!:0")
+
     else:
         print ("Ajoj, prava številka je nižja! :/")
+
