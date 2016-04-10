7500083c18b9feda5e4c524a802f160b77a36af7
Tanja Lavrencic <tancka.tancka@gmail.com>
2016-04-10 21:28:51 +0200

    domača naloga -trgovina

---
+++ trgovina.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+
+print ("Trenutno na voljo:")
+print ("mleko, cedevita, solata, lesniki, bonboni") #(kako/ali se lahko printa samo keys iz dictionary?)
+
+izdelki_cene = {"mleko": "1,15",
+        "cedevita": "5,56",
+        "solata": "7,09",
+        "lesniki": "3,45",
+        "bonboni": "2,15"
+}
+izbrani_izdelek = raw_input("Izberi izdelek:")
+def izpisi_ceno():
+        if izbrani_izdelek in izdelki_cene:
+            cena = izdelki_cene[izbrani_izdelek]
+            print "Cena tega izdelka znaša %s €"%(cena)
+
+        else:
+            print "Izdelek žal ni na voljo."
+if __name__ == "__main__":
+    izpisi_ceno()
+#zanka?
